3125178352a896232392eec7ea2df4a386ec2d54
uludagemre <uludag.emre.tr@gmail.com>
2023-10-29 00:49:39 +0200

    Add assertion for kinesis put recorsd.

--- kinesis_wrapper.py
+++ kinesis_wrapper.py
@@ -13,12 +13,19 @@ class KinesisWrapper:
             PartitionKey='some_key'
         )
 
-    def get_records(self, shard_iterator, limit, stream_arn):
-        # Simulating record retrieval from Kinesis
-        response = self.client.get_records(
-            ShardIterator=shard_iterator,
-            Limit=limit,
-            StreamARN=stream_arn
-        )
+    def consume_records(self):
+        shard_iterator = self.client.get_shard_iterator(
+            StreamName=self.stream_name,
+            ShardId='shardId-000000000000',  # Replace with your actual shard ID
+            ShardIteratorType='TRIM_HORIZON'
+        )['ShardIterator']
 
-        return response['Records']
+        records = []
+        while True:
+            response = self.client.get_records(ShardIterator=shard_iterator, Limit=10)
+            if not response['Records']:
+                break
+            records.extend(list(map(lambda x: x['Data'], response['Records'])))
+            shard_iterator = response['NextShardIterator']
+
+        return records
--- test_kinesis_module.py
+++ test_kinesis_module.py
@@ -8,18 +8,17 @@ class TestKinesisWithMoto(unittest.TestCase):
     @mock_kinesis
     def test_kinesis_put_and_get_records(self):
         stream_name = 'test-stream'
-
-        # Create the Kinesis stream
         kinesis = boto3.client('kinesis', region_name='us-east-1')
 
         kinesis.create_stream(StreamName=stream_name, ShardCount=1)
-
-        # Use KinesisWrapper
         kinesis_wrapper = KinesisWrapper(stream_name)
 
-        # Put records into the stream
         kinesis_wrapper.put_record('Test data 1')
         kinesis_wrapper.put_record('Test data 2')
+
+        records = kinesis_wrapper.consume_records()
+
+        assert records == [b'Test data 1', b'Test data 2']
 
 
 if __name__ == '__main__':
